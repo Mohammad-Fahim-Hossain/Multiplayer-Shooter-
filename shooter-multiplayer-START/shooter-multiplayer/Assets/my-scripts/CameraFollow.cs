ab11a911ed2ca1a1d48d1ea4842d978eacadc8d4
Mohammad-Fahim-Hossain <mohammad.fahim@northsouth.edu>
2020-12-19 01:45:47 +0600

    Import mirror asset for network, adding multiplayer network and HUD

--- shooter-multiplayer-START/shooter-multiplayer/Assets/my-scripts/CameraFollow.cs
+++ shooter-multiplayer-START/shooter-multiplayer/Assets/my-scripts/CameraFollow.cs
@@ -9,15 +9,19 @@ public class CameraFollow : MonoBehaviour {
 	Vector3 offset; // the distance between the main character and the camera
 	// Use this for initialization
 	void Start () {
-		offset = transform.position - Target.transform.position;
-	
+        if (Target != null)
+        {
+            offset = transform.position - Target.transform.position;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 targetCamera = Target.transform.position + offset;
+        if (Target != null)
+        {
+            Vector3 targetCamera = Target.transform.position + offset;
 
-		transform.position = Vector3.Lerp (transform.position, targetCamera, Time.deltaTime * cameraSmoothing);
-	
+            transform.position = Vector3.Lerp(transform.position, targetCamera, Time.deltaTime * cameraSmoothing);
+        }
 	}
 }
