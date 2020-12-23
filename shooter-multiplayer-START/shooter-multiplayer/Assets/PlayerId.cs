1375f97e73a3e616ecd13b302d784be5c0446148
Mohammad-Fahim-Hossain <mohammad.fahim@northsouth.edu>
2020-12-23 14:22:47 +0600

    Mordify Network identifier, Get NetID and Set UniquePlayerID.

---
+++ shooter-multiplayer-START/shooter-multiplayer/Assets/PlayerId.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerId : NetworkBehaviour
+{
+    [SyncVar] public string PlayerUniqueName;
+    private string PlayerNetID;
+
+    public override void OnStartLocalPlayer()
+    {
+        // get the network ID
+        //set the ID of the player
+
+        GetNetIdentity();
+        SetIdentity();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.name=="" || transform.name == "ironmanPrefab(Clone)")
+        {
+            SetIdentity();
+        }
+    }
+
+    [Client]
+    void GetNetIdentity()
+    {
+        PlayerNetID = GetComponent<NetworkIdentity>().netId.ToString();
+        //tell all the clients about the player ID
+        CmdTellServerMyIdentity(MakeUniqueIdentity());
+    }
+
+   void SetIdentity()
+    {
+        if (!isLocalPlayer)
+        {
+            this.transform.name = PlayerUniqueName;
+        }
+        else
+        {
+            this.transform.name = MakeUniqueIdentity();
+        }
+
+    }
+
+   public string MakeUniqueIdentity()
+    {
+        return "Player" + PlayerNetID;
+    }
+
+    [Command]
+    void CmdTellServerMyIdentity(string name)
+    {
+        PlayerUniqueName = name;
+    }
+}
